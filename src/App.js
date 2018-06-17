b13719d113e67269799ea041ca8f2428d1f4e1d2
Anya Craig <anya.craig@galepartners.com>
2018-06-17 10:44:25 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+class App extends Component {
+  async componentDidMount () {
+    try {
+      const response = await axios.post('/login')
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <p>Hello! Welcome to Paintbox!</p>
+      </div>
+    );		
+  }
+}
+
+export default App;
