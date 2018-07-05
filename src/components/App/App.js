eddf3f1bf45d731b464fc75cf80d6ecbbfe9b3e2
Anya Craig <AnyaCraig@users.noreply.github.com>
2018-07-05 17:00:43 -0400

    Added models, seeds, apis, and routing.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 
@@ -15,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hello! Welcome to Paintbox!</p>
+        <h1>PAINTBOX</h1>
       </div>
     );		
   }
